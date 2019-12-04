91abd0b846b053c0fc37303d3d06b0724e71c217
start4666 <start4666@gmail.com>
2019-12-03 17:31:20 -0700

    Lesson 1.9

--- sandbox/src/main/java/auto/sandbox/MyFirstPrg.java
+++ sandbox/src/main/java/auto/sandbox/MyFirstPrg.java
@@ -1,9 +1,19 @@
 package auto.sandbox;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MyFirstPrg {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("Alex");
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата cо cтороной " + s.l + " = " + s.area());
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прмоугольника cо cторонами " + r.a + " и " + r.b + " = " + r.area());
+  }
+
+    public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 
 }
---
+++ sandbox/src/main/java/auto/sandbox/Square.java
@@ -0,0 +1,11 @@
+package auto.sandbox;
+
+public class Square {
+  public double l;
+  public Square(double l) {
+    this.l = l;
+  }
+  public double area() {
+    return this.l * this.l;
+  }
+}

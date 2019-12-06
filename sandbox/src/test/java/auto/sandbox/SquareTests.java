7b83fe38dbc7a1e86ea52acfce7fa084b7998ff7
start4666 <start4666@gmail.com>
2019-12-05 18:14:43 -0700

    code refactoring

--- sandbox/src/test/java/auto/sandbox/SquareTests.java
+++ sandbox/src/test/java/auto/sandbox/SquareTests.java
@@ -1,11 +1,12 @@
 package auto.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
 @Test
     public void testArea() {
     Square s = new Square(5);
-    assert s.area() == 25;
+    Assert.assertEquals(s.area(), 25);
   }
 }

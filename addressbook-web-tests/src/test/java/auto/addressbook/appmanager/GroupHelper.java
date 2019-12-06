311efaa539952ee5f0b568847051ba7efed6675c
start4666 <start4666@gmail.com>
2019-12-06 14:00:32 -0700

    lesson 2.9

--- addressbook-web-tests/src/test/java/auto/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/auto/addressbook/appmanager/GroupHelper.java
@@ -31,4 +31,8 @@ public class GroupHelper extends HelperBase {
 
   public void deleteSelectedGroup() { click(By.name("delete")); }
 
+  public void initGroupModification() { click(By.name("edit"));  }
+
+  public void submitGroupModification() { click(By.name("update"));  }
+
 }
--- addressbook-web-tests/src/test/java/auto/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/auto/addressbook/tests/GroupModificationTests.java
@@ -1,12 +1,17 @@
 package auto.addressbook.tests;
 
+import auto.addressbook.model.GroupData;
 import org.junit.Test;
 
 public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification(){
-    app.gotoGroupPage();
+    app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test2", "test", "test"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 }

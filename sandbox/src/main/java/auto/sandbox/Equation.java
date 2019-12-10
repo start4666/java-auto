9749655a36f7ec99452845b8023a60cdabc66fef
start4666 <start4666@gmail.com>
2019-12-09 20:22:17 -0700

    Lesson 3.2 finished

--- sandbox/src/main/java/auto/sandbox/Equation.java
+++ sandbox/src/main/java/auto/sandbox/Equation.java
@@ -15,17 +15,7 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n = 0;
-        }
-      } else {
-        n = 1;
-      }
-    } else {
+    if (a != 0) {
       if (d > 0) {
         n = 2;
       } else if (d == 0) {
@@ -33,7 +23,12 @@ public class Equation {
       } else {
         n = 0;
       }
-
+    } else if (b != 0) {
+      n = 1;
+    } else if (c != 0) {
+      n = 0;
+    } else {
+      n = -1;
     }
 
   }
